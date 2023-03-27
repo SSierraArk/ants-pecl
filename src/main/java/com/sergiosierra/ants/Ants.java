ab3b86292fbda44873ccee9a0b5292bad624fdf5
SSierraArk <ssierra@will-soft.net>
2023-03-27 18:06:07 +0200

    First commit #3-#4

---
+++ src/main/java/com/sergiosierra/ants/Ants.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sergiosierra.ants;
+
+/**
+ *
+ * @author ssierra
+ */
+public class Ants {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
