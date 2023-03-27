1e121e30cc23cf6b99c13c24cda2e8281834f77a
SSierraArk <ssierra@will-soft.net>
2023-03-27 20:27:01 +0200

    Designed server-side views #1

--- src/main/java/com/sergiosierra/ants/views/serverView.java
+++ src/main/java/com/sergiosierra/ants/views/serverView.java
@@ -249,7 +249,7 @@ public class serverView extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
