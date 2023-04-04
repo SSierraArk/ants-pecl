62d75e7b8526097822766608458ee14a91093a69
SSierraArk <ssierra@will-soft.net>
2023-04-04 15:43:01 +0200

    Creatted logger class and method. #20

---
+++ src/main/java/com/sergiosierra/ants/helpers/Logger.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sergiosierra.ants.helpers;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author ssierra
+ */
+public class Logger {
+    
+    public static synchronized void logln(String str) throws IOException {
+    
+        Date now = new Date();
+        String format = "HH:mm:ss dd-MM-yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        
+        String formattedContent = sdf.format(now) + " - " + str;
+        
+        
+        System.out.println(str);
+        
+        
+        FileWriter fw = new FileWriter("./", true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(formattedContent);
+        bw.newLine();
+        bw.close();
+        fw.close();
+        
+        
+    }
+    
+}
